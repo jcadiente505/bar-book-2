bd33bfb3700e0177819c692640bc670c0874d0be
Cadman <jcadiente505@gmail.com>
2018-10-16 17:44:15 -0400

    heroku deployment

---
+++ client/build/precache-manifest.5022d919e30fbf6cb8d2494cafb5b5b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "42a7e33385580c4814d5f8653d1e6504",
+    "url": "/static/media/recipe-photo.42a7e333.jpeg"
+  },
+  {
+    "revision": "603d326d61e464de07e9a823b6942f72",
+    "url": "/static/media/forum-photo.603d326d.jpeg"
+  },
+  {
+    "revision": "28d20659bc5dee903f71fb87760adc95",
+    "url": "/static/media/cocktail.28d20659.jpg"
+  },
+  {
+    "revision": "fa19af8ee1a01cb47a3df69287192be7",
+    "url": "/static/media/cocktail-photo.fa19af8e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7f9052fe30c6b9cea45",
+    "url": "/static/js/main.b7f9052f.chunk.js"
+  },
+  {
+    "revision": "b05cc7d24c8cce9894e2",
+    "url": "/static/js/1.b05cc7d2.chunk.js"
+  },
+  {
+    "revision": "b7f9052fe30c6b9cea45",
+    "url": "/static/css/main.49681921.chunk.css"
+  },
+  {
+    "revision": "1c894f4ae7c84e28cc66a8403542cf30",
+    "url": "/index.html"
+  }
+];
